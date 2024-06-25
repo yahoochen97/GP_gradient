00a350dfa18e0942881c4ac95f68d72e4c4c68cc
Yehu Chen <chenyehu@wustl.edu>
2024-06-25 10:16:08 -0500

    add trump hate py

--- TrumpHate/TrumpHate_GPR.py
+++ TrumpHate/TrumpHate_GPR.py
@@ -31,7 +31,7 @@ class GPModel(gpytorch.models.ExactGP):
     def __init__(self, train_x, train_y, likelihood):
         super().__init__(train_x, train_y, likelihood)
         # constant country-level mean
-        self.mean_module = LinearMean(input_size=train_x.shape[1], bias=True)
+        self.mean_module = LinearMean(input_size=train_x.shape[1], bias=False)
         self.covar_module = ScaleKernel(RBFKernel(ard_num_dims=train_x.shape[1]))
     
     def forward(self, x):
@@ -138,7 +138,7 @@ likelihood.eval()
 model.covar_module.outputscale = 0.1
 
 xss = xs.clone().detach().requires_grad_(False)
-xss[:,1] = 0
+# xss[:,1] = 0
 xss[:,2] = 0
 with torch.no_grad():
     out0 = model(xss)
