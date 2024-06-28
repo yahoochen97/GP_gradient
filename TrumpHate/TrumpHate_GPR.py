1671f7ec6ca4e0c93eec95afc79f8fa5d645e965
Yehu Chen <chenyehu@wustl.edu>
2024-06-28 14:23:23 -0500

    add exile py

--- TrumpHate/TrumpHate_GPR.py
+++ TrumpHate/TrumpHate_GPR.py
@@ -60,7 +60,7 @@ model = GPModel(xs, ys, likelihood).double()
 
 # initialize model parameters
 hypers = {
-    'mean_module.weights': torch.tensor(x_weights), #
+    'mean_module.weights': torch.tensor([0,0,0]), #
     'covar_module.outputscale': torch.var(ys),
     'covar_module.base_kernel.lengthscale': torch.tensor([90.,1, 90]),
     'likelihood.noise': 1,
@@ -75,7 +75,7 @@ likelihood.train()
 all_params = set(model.parameters())
 model.covar_module.base_kernel.raw_lengthscale.requires_grad = False
 # model.covar_module.raw_outputscale.requires_grad = False
-# model.mean_module.weights.requires_grad = False
+model.mean_module.weights.requires_grad = False
 optimizer = torch.optim.Adam(all_params, lr=0.05)
 
 # "Loss" for GPs - the marginal log likelihood
