d44e7d44ccc8207dfbbeda5e456b58eb93f9fb07
Yehu Chen <chenyehu@wustl.edu>
2024-06-28 15:20:18 -0500

    add exile py

--- TrumpHate/TrumpHate_GPR.py
+++ TrumpHate/TrumpHate_GPR.py
@@ -33,13 +33,13 @@ class GPModel(gpytorch.models.ExactGP):
     def __init__(self, train_x, train_y, likelihood):
         super().__init__(train_x, train_y, likelihood)
         # constant country-level mean
-        # self.mean_module = LinearMean(input_size=train_x.shape[1], bias=False)
-        self.mean_module = LinearMean(input_size=(1), bias=True)
+        self.mean_module = LinearMean(input_size=train_x.shape[1], bias=True)
+        # self.mean_module = LinearMean(input_size=(1), bias=True)
         # self.covar_module = ScaleKernel(RBFKernel(ard_num_dims=train_x.shape[1]))
         self.covar_module = ScaleKernel(RBFKernel(active_dims=[0]))
     
     def forward(self, x):
-        mean_x = self.mean_module(x[:,1])
+        mean_x = self.mean_module(x)
         covar_x = self.covar_module(x)
         
         return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
@@ -61,7 +61,7 @@ model = GPModel(xs, ys, likelihood).double()
 
 # initialize model parameters
 hypers = {
-    'mean_module.weights': torch.tensor([0]), #
+    'mean_module.weights': torch.tensor([0,0,0]), #
     'covar_module.outputscale': 1,
     'covar_module.base_kernel.lengthscale': torch.tensor([90.]), # 90.,10, 90
     'likelihood.noise': 1,
@@ -159,7 +159,7 @@ effect_std = np.sqrt((out1.variance.detach().numpy()[mask1]\
                       + out0.variance.detach().numpy()[mask0]))[0]
 
 print("instaneous shift on Election Day: {:.2E} +- {:.2E}\n".format(effect/ys_scale, effect_std/ys_scale))
-BIC = (1+1+1)*torch.log(torch.tensor(xs.size(0))) + 2*loss*xs.size()[0]
+BIC = (3+1+1)*torch.log(torch.tensor(xs.size(0))) + 2*loss*xs.size()[0]
 print(norm.cdf(-np.abs(effect/effect_std)))
 print("log lik: {:4.4f} \n".format(-loss.numpy()*xs.size(0)))
 print("BIC: {:0.3f} \n".format(BIC))
